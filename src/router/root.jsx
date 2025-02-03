390784907e26ce1182209eac086ce91b6b30bdaf
dongjun <edj0707@naver.com>
2025-02-03 22:20:01 +0900

    feat: outlet으로 독립된 컴포넌트 만듬

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,15 +1,17 @@
-import { Suspense, lazy } from "react";
+import { Children, Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-const loading = <div className="bg-red-700">Loading....</div>;
+const Loading = <div className="bg-red-700">Loading....</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
   {
     path: "",
     element: (
-      <Suspense fallback={loading}>
+      <Suspense fallback={Loading}>
         <Main />
       </Suspense>
     ),
@@ -17,10 +19,29 @@ const root = createBrowserRouter([
   {
     path: "about",
     element: (
-      <Suspense fallback={loading}>
+      <Suspense fallback={Loading}>
         <About />
       </Suspense>
     ),
+  },
+  {
+    path: "todo",
+    element: (
+      <Suspense fallback={Loading}>
+        <TodoIndex />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "list",
+        element: (
+          <Suspense fallback={Loading}>
+            {" "}
+            <TodoList />{" "}
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
 
