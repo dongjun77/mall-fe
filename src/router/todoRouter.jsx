98ce50e61d3698e60765c9e198ebaaf13cea41ac
dongjun <edj0707@naver.com>
2025-02-04 17:14:27 +0900

    feat: search params로 url의 search 동적으로 유지하기

--- src/router/todoRouter.jsx
+++ src/router/todoRouter.jsx
@@ -5,20 +5,49 @@ const Loading = <div className="bg-red-700">Loading....</div>;
 
 const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
+const TodoRead = lazy(() => import("../pages/todo/ReadPage"));
+
+const TodoAdd = lazy(() => import("../pages/todo/AddPage"));
+
+const TodoModify = lazy(() => import("../pages/todo/ModifyPage"));
+
 function todoRouter() {
   return [
     {
       path: "list",
       element: (
         <Suspense fallback={Loading}>
-          {" "}
-          <TodoList />{" "}
+          <TodoList />
         </Suspense>
       ),
     },
     {
       path: "",
       element: <Navigate replace={true} to={"list"} />,
+    },
+    {
+      path: "read/:tno",
+      element: (
+        <Suspense fallback={Loading}>
+          <TodoRead />
+        </Suspense>
+      ),
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={Loading}>
+          <TodoAdd />
+        </Suspense>
+      ),
+    },
+    {
+      path: "modify/:tno",
+      element: (
+        <Suspense fallback={Loading}>
+          <TodoModify />
+        </Suspense>
+      ),
     },
   ];
 }
