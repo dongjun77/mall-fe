7d2fdc89edc45f82a0056dbbcc16632862042376
dongjun <edj0707@naver.com>
2025-02-06 12:46:53 +0900

    feat: axios init api서버와 통신하기

--- src/pages/todo/ReadPage.jsx
+++ src/pages/todo/ReadPage.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   createSearchParams,
   useNavigate,
   useParams,
   useSearchParams,
 } from "react-router-dom";
+import ReadComponent from "../../components/todo/ReadComponent";
 
 function ReadPage(props) {
   const navigate = useNavigate();
@@ -31,12 +32,9 @@ function ReadPage(props) {
   }, [page, size]);
 
   return (
-    <div className={"text-3xl"}>
-      Todo Read Page {tno}
-      <div>
-        <button onClick={() => moveToModify(tno)}>Test Modify</button>
-        <button onClick={() => moveToList()}>Test List</button>
-      </div>
+    <div className={"font-extrabold w-full bg-white mt-6"}>
+      <div className={"text-2xl"}>Todo Read Page Component{tno}</div>
+      <ReadComponent tno={tno} />
     </div>
   );
 }
