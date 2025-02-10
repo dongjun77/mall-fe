4c371faf14bff3f7eaedbca9e603cbeecebae22b
dongjun <edj0707@naver.com>
2025-02-10 21:43:49 +0900

    feat: /Products 하위 라우팅설정

--- src/router/productsRouter.jsx
+++ src/router/productsRouter.jsx
@@ -1,7 +1,24 @@
 import React, { lazy, Suspense } from "react";
+import { Navigate } from "react-router-dom";
+
+const Loading = <div className="bg-red-700">Loading....</div>;
+const ProductList = lazy(() => import("../pages/products/ListPage"));
 
 const productsRouter = () => {
-  return [];
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={Loading}>
+          <ProductList />
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace to={"/products/list"}></Navigate>,
+    },
+  ];
 };
 
 export default productsRouter;
