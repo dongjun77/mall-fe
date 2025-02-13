748474ea1a8bacb80ac56707c73bb119af3d53af
dongjun <edj0707@naver.com>
2025-02-13 20:14:08 +0900

    feat: cookieUtil 사용자 정보 저장

--- src/slices/loginSlice.jsx
+++ src/slices/loginSlice.jsx
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { loginPost } from "../api/memberApi";
+import { setCookie } from "../util/cookieUtil";
 
 const initState = {
   email: "",
@@ -32,6 +33,10 @@ const loginSlice = createSlice({
         console.log("fulfilled");
 
         const payload = action.payload;
+
+        if (!payload.error) {
+          setCookie("member", JSON.stringify(payload), 1);
+        }
 
         return payload;
       })
