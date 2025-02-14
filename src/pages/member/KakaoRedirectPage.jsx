c70c388354039c0621b52104fb91aa997e47ab00
dongjun <edj0707@naver.com>
2025-02-14 17:32:32 +0900

    feat: kakao access token 하기

--- src/pages/member/KakaoRedirectPage.jsx
+++ src/pages/member/KakaoRedirectPage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { getAccessToken } from "../../api/kakaoApi";
 
 const KakaoRedirectPage = () => {
   const [searchParams] = useSearchParams();
 
   const authCode = searchParams.get("code");
+
+  useEffect(() => {
+    getAccessToken(authCode).then((data) => {
+      console.log(data);
+    });
+  }, [authCode]);
 
   return (
     <div>
