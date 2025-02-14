0157bbdd41d4b1b250fc020e4908ac223e0ae9a3
dongjun <edj0707@naver.com>
2025-02-14 22:07:39 +0900

    feat: 소셜로그인시 route 처리, login 토큰받아서 쿠키에 저장

--- src/pages/member/KakaoRedirectPage.jsx
+++ src/pages/member/KakaoRedirectPage.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
+import { useDispatch } from "react-redux";
+import { login } from "../../slices/loginSlice";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const KakaoRedirectPage = () => {
   const [searchParams] = useSearchParams();
 
+  const { moveToPath } = useCustomLogin();
+
   const authCode = searchParams.get("code");
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getAccessToken(authCode).then((accessToken) => {
-      getMemberWithAccessToken(accessToken).then((result) => {
+      getMemberWithAccessToken(accessToken).then((memberInfo) => {
         console.log("----------------------------");
-        console.log(result);
+        console.log(memberInfo);
+        dispatch(login(memberInfo));
+
+        if (memberInfo && memberInfo.social) {
+          moveToPath("/member/modify");
+        } else {
+          moveToPath("/");
+        }
       });
     });
   }, [authCode]);
