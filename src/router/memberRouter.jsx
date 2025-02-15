bebf8a45adb8dfac1c8188b9e76de634cb511772
dongjun <edj0707@naver.com>
2025-02-15 11:12:53 +0900

    feat: 회원정보 수정 페이지

--- src/router/memberRouter.jsx
+++ src/router/memberRouter.jsx
@@ -8,6 +8,8 @@ const Login = lazy(() => import("../pages/member/LoginPage"));
 const Logout = lazy(() => import("../pages/member/LogoutPage"));
 const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"));
 
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"));
+
 const memberRouter = () => {
   return [
     {
@@ -31,6 +33,14 @@ const memberRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <KakaoRedirect />
+        </Suspense>
+      ),
+    },
+    {
+      path: "modify",
+      element: (
+        <Suspense fallback={Loading}>
+          <MemberModify />
         </Suspense>
       ),
     },
